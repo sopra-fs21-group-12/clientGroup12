6313cf8fac1bc44add0e226792459b1aa57c5d3a
otopalak <onur.topalak@uzh.ch>
2021-04-08 00:09:10 +0200

    material-ui version funktioniert, rs verison nid
    dr TagPicker isch mitem sessionStorge in sync damit meh spöter eifach uf die selected tags cha zuegriffe

--- src/TestPage.js
+++ src/TestPage.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-//import { Button } from '../../views/design/Button';
-import { FlexboxGrid, Panel, Form, FormGroup, ControlLabel, FormControl, ButtonToolbar, Button } from 'rsuite'; 
 
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Input, InputLabel, MenuItem, FormControl, Select, Chip, Button } from '@material-ui/core';
+import TagPicker from './components/tagPicker/TagPicker';
+import TagPickerRS from './components/tagPicker/TagPickerRS';
 
 class TestPage extends React.Component {
   render() {
     return (
-        <FlexboxGrid justify="center">
-        <FlexboxGrid.Item colspan={12}>
-          <Panel header={<h3>Login</h3>} bordered>
-            <Form fluid>
-              <FormGroup>
-                <ControlLabel>Username or email address</ControlLabel>
-                <FormControl name="name" />
-              </FormGroup>
-              <FormGroup>
-                <ControlLabel>Password</ControlLabel>
-                <FormControl name="password" type="password" />
-              </FormGroup>
-              <FormGroup>
-                <ButtonToolbar>
-                  <Button appearance="primary">Sign in</Button>
-                  <Button appearance="link">Forgot password?</Button>
-                </ButtonToolbar>
-              </FormGroup>
-            </Form>
-          </Panel>
-        </FlexboxGrid.Item>
-      </FlexboxGrid>
+      <div>
+      <TagPicker/>
+      <Button variant="contained" onClick={() => alert(sessionStorage.getItem("selectedTags"))}>Show selected Tags</Button>
+      </div>
     );
   }
 }
