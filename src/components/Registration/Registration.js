62cc337069179aadf627bafa878d6fd5837b1df4
filiptrenuzh <filip.trendafilov@uzh.ch>
2021-04-27 16:07:24 +0200

    registration page refactoring to hooks with authentication implementations

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
+import {register} from "../../actions/AAuth";
 import {useHistory, withRouter} from 'react-router-dom';
 import {Avatar, Button, Container, Grid, makeStyles, TextField, Typography, Paper} from "@material-ui/core";
 import {api, handleError} from "../../helpers/api";
 import User from "../shared/models/User";
+
+const required = (value) => {
+  if(!value) {
+    return (
+      <div className={"alert alert-danger"} role="alert">
+        This field is required!
+      </div>
+    )
+  }
+}
+
+const validUsername = (value) => {
+  if (value.length < 3 || value.length > 20) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        this username must be between 3 and 20 characters.
+      </div>
+    )
+  }
+}
+
+const validPassword = (value) => {
+  if (value.length < 6 || value.length > 40) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        The password must be between 6 and 40 characters.
+      </div>
+    );
+  }
+};
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -26,15 +57,40 @@ const useStyles = makeStyles((theme) => ({
 
 function Registration() {
   const classes = useStyles();
+
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postCode, setPostCode] = useState();
+  const [successful, setSuccessful] = useState(false);
+
+  const onChangeUsername = (input) => {
+    const username = input.target.value;
+    setUsername(username);
+  };
+
+  const onChangeName = (input) => {
+    const name = input.target.value;
+    setName(name);
+  };
+
+  const onChangePassword = (input) => {
+    const password = input.target.value;
+    setPassword(password);
+  }
   const history = useHistory();
   const handleRegistration = async () => {
     try {
       // What we want to send back to the backend
       const requestBody = JSON.stringify({
-        username: this.state.username.toLowerCase(),
-        name: this.state.name,
-        password: this.state.password,
-        birthday: this.state.birthday
+        username: username.toLowerCase(),
+        name: name,
+        password: password,
+        address: address,
+        city: city,
+        postCode: postCode,
       });
       // Post request to the backend with given data
       const response = await api.post('/users', requestBody);
@@ -48,8 +104,9 @@ function Registration() {
       localStorage.setItem('username',user.username);
       localStorage.setItem('name',user.name);
 
-      // Login successfully worked --> navigate to the route /game in the GameRouter,
+      // registration successfully  --> navigate to the route /game in the GameRouter,
       // If something went wrong send the user back to the registration
+      // TODO: tobe refactored
       this.props.history.push(`/game`);
     } catch (error) {
       this.props.history.push("/registration")
