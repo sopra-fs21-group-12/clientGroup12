8a0e8b622e83fe4838535418fed933ce162cea8c
otopalak <onur.topalak@uzh.ch>
2021-04-30 11:06:45 +0200

    picture upload modal in itemupload

--- src/components/item/ItemUpload.js
+++ src/components/item/ItemUpload.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Typography, TextField, Button, Link } from '@material-ui/core'
-import { Panel } from 'rsuite'
+import {Button as RsuiteButton, Modal, Panel, Uploader} from 'rsuite'
 
 import TagPickerRS from '../tagPicker/TagPickerRS'
 import {api, handleError} from "../../helpers/api";
@@ -30,6 +30,11 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
+const dropzone = {
+    lineHeight: '200px'
+};
+
+
 function ItemUpload() {
     const history = useHistory();
     const classes = useStyles();
@@ -42,6 +47,9 @@ function ItemUpload() {
     const [image, setImage] = useState();
     const [selectedTags, setTags] = useState();
     const [tags, setAvailableTags] = useState([]);
+
+    const [modal, setModal] = useState({show: false});
+    const [itemId, setItemId] = useState();
 
     function handleTags(newValue){
         setTags(newValue);
@@ -89,75 +97,104 @@ function ItemUpload() {
                 description: state.description,
                 tagsItem: selectedTags
             });
-            await api.post(`/users/${localStorage.getItem("id")}/items`, requestBody);
+            const response = await api.post(`/users/${localStorage.getItem("id")}/items`, requestBody);
+            setItemId(response.data.id);
+            open();
 
-            history.push('/game')
         } catch (error) {
             alert(`Something went wrong during the login: \n${handleError(error)}`);
         }
     }
 
+    function close() {
+        setModal({show: false});
+        history.push('/inventory')
+    }
+
+    function open() {
+        setModal({show: true});
+    }
+
     return (
         <Grid container justify="center" spacing={4}>
-        <Grid item xs={12}/>
-        <Grid item xs={12}/>
+            <Grid item xs={12}/>
+            <Grid item xs={12}/>
 
-        <Grid item xs={6}>
-          <Panel shaded>
-            <Typography variant="h5">Add new Item to your Inventory</Typography>
+            <Grid item xs={6}>
+              <Panel shaded>
+                <Typography variant="h5">Add new Item to your Inventory</Typography>
 
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="title"
-              label="Item Title"
-              name="title"
-              autoFocus
-              onChange={handleChange}
-            />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="title"
+                  label="Item Title"
+                  name="title"
+                  autoFocus
+                  onChange={handleChange}
+                />
 
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="description"
-              label="Item Description"
-              id="description"
-              rows={5}
-              multiline
-              onChange={handleChange}
-            />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="description"
+                  label="Item Description"
+                  id="description"
+                  rows={5}
+                  multiline
+                  onChange={handleChange}
+                />
 
-              <TagPickerRS value={selectedTags} onChange={handleTags} tags={tags}/>
+                  <TagPickerRS value={selectedTags} onChange={handleTags} tags={tags}/>
 
-            <Grid container justify="flex-start" alignItems="stretch">
-              <Grid item xs={12}>
-                <Button
-                  disabled={!state.title || !state.description || !selectedTags}
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  className={classes.submit}
-                  onClick={handleSave}
-                >
-                Save
-                </Button>
-              </Grid>
-                <Grid item xs={12}>
-                    <Button
-                        variant="outlined"
-                        color="default"
-                    >
-                        Cancel
-                    </Button>
+                <Grid container justify="flex-start" alignItems="stretch">
+                  <Grid item xs={12}>
+                            <Button
+                              disabled={!state.title || !state.description || !selectedTags}
+                              type="submit"
+                              variant="contained"
+                              color="primary"
+                              className={classes.submit}
+                              onClick={handleSave}
+                            >
+                            Save
+                            </Button>
+                  </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            variant="outlined"
+                            color="default"
+                        >
+                            Cancel
+                        </Button>
+                    </Grid>
                 </Grid>
+              </Panel>
             </Grid>
-          </Panel>
-        </Grid>
-  
+
+            <Modal show={modal.show} onHide={close} backdrop="static">
+                <Modal.Header closeButton={false}>
+                    <Modal.Title>Set picture for your item (required)</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                    <Uploader
+                        action={`http://sopra-fs21-group-12-server.herokuapp.com/items/${itemId}/pictures/upload`}
+                        draggable
+                        onSuccess={close}
+                    >
+                        <div style={dropzone}>Click or Drag files to this area to upload</div>
+                    </Uploader>
+
+                </Modal.Body>
+                <Modal.Footer>
+                </Modal.Footer>
+            </Modal>
+
       </Grid>
     )
 }
