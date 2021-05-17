6b91109067fcfceb1a8cbe9aa966b6696894ccd0
otopalak <onur.topalak@uzh.ch>
2021-05-17 22:14:03 +0200

    Refine Chat design

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,8 +7,9 @@ import { isProduction } from './isProduction';
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = 'https://sopra-fs21-group-12-server.herokuapp.com'; // TODO: insert your groups heroku prod url for server (once deployed)
-  const devUrl =  'http://localhost:8080';
+  const prodUrl = 'http://sopra-fs21-group-12-server.herokuapp.com'; // TODO: insert your groups heroku prod url for server (once deployed)
+  //const devUrl =  'http://localhost:8080';
+  const devUrl =  'http://sopra-fs21-group-12-server.herokuapp.com';
 
 
   return isProduction() ? prodUrl : devUrl;
